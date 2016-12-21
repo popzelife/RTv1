16fd4a7797d467ca6a22a6505ed7a25252f8481b
Quentin DUPERON <qduperon@e1r8p6.42.fr>
2016-12-21 19:53:56 +0100

    update

--- srcs/parsing/camera.c
+++ srcs/parsing/camera.c
@@ -6,7 +6,7 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 16:31:07 by qduperon          #+#    #+#             */
-/*   Updated: 2016/12/20 17:51:22 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/12/21 19:52:59 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,22 @@
 
 t_cam	*init_camera(char **file, int i, t_cam *cam)
 {
+	t_vec3	look_from;
+	t_vec3	look_at;
+	float	fov;
+	float	aperture;
+
 	while (!ft_strstr(file[i], "-------"))
 	{
+		if (ft_strstr(file[i]), "pos:")
+			look_from = ft_vector(line[i + 1]);
+		if (ft_strstr(file[i]), "lookat:")
+			look_at = ft_vector(line[i + 1]);
 		if (ft_strstr(file[i]), "fov:")
+			fov = ft_data(line[i + 1]);
+		if (ft_strstr(file[i]), "aperture:")
+			aperture = ft_data(line[i + 1]);
+		i++;
 	}
+	return (cam);
 }
--- srcs/parsing/tools.c
+++ srcs/parsing/tools.c
@@ -6,11 +6,28 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 16:06:40 by qduperon          #+#    #+#             */
-/*   Updated: 2016/12/20 16:19:19 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/12/21 19:25:26 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/rtv1.h"
+
+float	ft_data(char *line)
+{
+	return ((float)ft_atodouble(&line));
+}
+
+t_vec3	ft_vector(char *line)
+{
+	float	x;
+	float	y;
+	float	z;
+
+	x = (float)ft_atodouble(&line);
+	y = (float)ft_atodouble(&line);
+	z = (float)ft_atodouble(&line);
+	return (v3(x, y, z));
+}
 
 int		ft_countobj(char **file)
 {

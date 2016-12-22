0477317047c5d21812645ac06133803843d0f649
Quentin DUPERON <qduperon@e1r9p6.42.fr>
2016-12-22 18:27:51 +0100

    update parsing

--- srcs/parsing/camera.c
+++ srcs/parsing/camera.c
@@ -6,30 +6,54 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 16:31:07 by qduperon          #+#    #+#             */
-/*   Updated: 2016/12/21 19:52:59 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/12/22 18:25:34 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/rtv1.h"
 
-t_cam	*init_camera(char **file, int i, t_cam *cam)
+typedef struct 	s_c
 {
-	t_vec3	look_from;
-	t_vec3	look_at;
-	float	fov;
-	float	aperture;
+	t_vec3		look_f;
+	t_vec3		look_at;
+	float		fov;
+	float		ap;
+}				t_c;
+
+t_cam	*init_camera2(t_c c, t_cam *cam, t_rt *rt)
+{
+	t_vec3	*orig;
+	t_vec3	*u;
+	t_vec3	*v;
+	t_vec3	*w;
+	float	half_height;
+	float	half_width;
+	float	lens_radius;
+
+	half_height = tan((c.fov * M_PI / 180) / 2.0);
+	half_width = ((float)rt->r_view->w / (float)rt->r_view->h) * half_height;
+	lens_radius = c.ap / 2;
+	orig = v3_copy_vec(c.look_f);
+	w = v3_unit_vec(v3_sub_vec(c.look_f, c.look_at));
+	u = v3_unit_vec(v3_cross_vec(, w));
+}
+
+t_cam	*init_camera(char **file, int i, t_cam *cam, t_rt *rt)
+{
+	t_c c;
 
 	while (!ft_strstr(file[i], "-------"))
 	{
 		if (ft_strstr(file[i]), "pos:")
-			look_from = ft_vector(line[i + 1]);
+			c.look_f = ft_vector(line[i + 1]);
 		if (ft_strstr(file[i]), "lookat:")
-			look_at = ft_vector(line[i + 1]);
+			c.look_at = ft_vector(line[i + 1]);
 		if (ft_strstr(file[i]), "fov:")
-			fov = ft_data(line[i + 1]);
+			c.fov = ft_data(line[i + 1]);
 		if (ft_strstr(file[i]), "aperture:")
-			aperture = ft_data(line[i + 1]);
+			c.ap = ft_data(line[i + 1]);
 		i++;
 	}
+	cam = init_camera2(c, cam, rt);
 	return (cam);
 }
--- srcs/parsing/init.c
+++ srcs/parsing/init.c
@@ -6,13 +6,13 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 15:39:32 by qduperon          #+#    #+#             */
-/*   Updated: 2016/12/20 16:30:56 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/12/22 18:16:45 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/rtv1.h"
 
-t_scene		*init_scene(char *scene)
+t_scene		*init_scene(char *scene, t_rt *rt)
 {
 	char	**file;
 	int		i;
@@ -27,7 +27,7 @@ t_scene		*init_scene(char *scene)
 	while(!ft_strstr(file[i], "END"))
 	{
 		if (ft_strstr(file[i], "camera:"))
-			cam = init_camera(file, i, cam);
+			cam = init_camera(file, i, cam, rt);
 
 	}
 	free_tab(file, i);

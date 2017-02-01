b4025228394b37a946b301f6c0081f7c4fb4045c
Pierre MARTINEAU <pmartine@e2r7p21.42.fr>
2017-02-01 17:27:43 +0100

    2

--- srcs/rt_cylinder.c
+++ srcs/rt_cylinder.c
@@ -66,6 +66,7 @@ BOOL	hit_cylinder(void *obj, const t_ray *ray, const double t_min, \
 			v3_free(set);
 			v = v3_sub_vec(*(param->pos), *(cyl->center));
 			set = v3_div_vec(*v, cyl->radius);
+			get_normal(set, obj, param->pos);
 			v3_set(param->normal, set->x, set->y, set->z);
 			v3_free(set);
 			v3_free(v);
@@ -80,7 +81,7 @@ BOOL	hit_cylinder(void *obj, const t_ray *ray, const double t_min, \
 			v3_free(set);
 			v = v3_sub_vec(*(param->pos), *(cyl->center));
 			set = v3_div_vec(*v, cyl->radius);
-			get_normal(param->normal, param, param->pos);
+			get_normal(set, obj, param->pos);
 			v3_set(param->normal, set->x, set->y, set->z);
 			v3_free(set);
 			v3_free(v);
